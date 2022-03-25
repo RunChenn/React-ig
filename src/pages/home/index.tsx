5508287597e38eeaad9c1c55f0a8da64d202da03
runchen <yma8882003@gmail.com>
2022-03-25 18:58:47 +0800

    add view

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,99 @@
+import Header from 'components/Header';
+import Container from 'components/Container';
+import Story from './components/Story';
+import User from 'components/User';
+import Post from './components/Post';
+import Profile from './components/Profile';
+
+const PostList: React.FC = () => {
+  // const { data, isLoading } = useGetIGPostsQuery('all');
+
+  const data = [
+    {
+      id: 1,
+      location: '布魯斯前端',
+      account: 'bruce_1234',
+      avatar: '/images/avatars/a1.png',
+      photo: '/images/posts/main1.png',
+      likes: 999,
+      description: '我的老天鵝！',
+      hashTags: '#鵝鵝',
+      createTime: '1 HOURS AGO',
+    },
+    {
+      id: 2,
+      location: '7-11你好門市',
+      account: '__0831_xxx__',
+      avatar: '/images/avatars/a2.png',
+      photo: '/images/posts/main2.png',
+      likes: 333,
+      description: '我的老天鵝！',
+      hashTags: '#鵝鵝',
+      createTime: '5 HOURS AGO',
+    },
+    {
+      id: 3,
+      location: 'Taipei',
+      account: 'gogogo_0214',
+      avatar: '/images/avatars/a3.png',
+      photo: '/images/posts/main3.png',
+      likes: 777,
+      description: '我的老天鵝！',
+      hashTags: '#鵝鵝',
+      createTime: '6 HOURS AGO',
+    },
+    {
+      id: 4,
+      location: '信義區',
+      account: 'zoe_0000',
+      avatar: '/images/avatars/a4.png',
+      photo: '/images/posts/main4.png',
+      likes: 824,
+      description: '我的老天鵝！',
+      hashTags: '#鵝鵝',
+      createTime: '7 HOURS AGO',
+    },
+    {
+      id: 5,
+      location: '南港區',
+      account: 'zoe_0000',
+      avatar: '/images/avatars/a5.png',
+      photo: '/images/posts/main5.png',
+      likes: 98,
+      description: '我的老天鵝！',
+      hashTags: '#鵝鵝',
+      createTime: '10 HOURS AGO',
+    },
+  ];
+
+  return (
+    <>
+      {data?.map((item) => {
+        const { id, location, account, avatar, photo, likes, description, hashTags, createTime } = item;
+        return <Post location={location} account={account} avatar={avatar} photo={photo} likes={likes} description={description} hashTags={hashTags} createTime={createTime} key={id} />;
+      })}
+    </>
+  );
+};
+
 const Home: React.FC = () => {
-  return <p>Home</p>;
+  return (
+    <>
+      <Header />
+      <Container>
+        <div className="flex lg:justify-center">
+          <div className="w-full lg:w-[600px]">
+            <Story />
+            <PostList />
+          </div>
+          <div className="hidden lg:block lg:w-[424px]">
+            {/* <User avatar="/images/avatars/a9.png" size="medium" account="user1" location="Taipei" showFollow={true} isFollowing={false} /> */}
+            <Profile />
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default Home;
